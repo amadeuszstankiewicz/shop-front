0f848b0886ec557dbe0684f8636556f1f8b9feff
amadeuszstankiewicz <amadeusz.stankiewicz@frogriot.com>
2023-07-24 00:22:25 +0200

    [fix] icons in header and product brick

--- components/ProductBrick.js
+++ components/ProductBrick.js
@@ -25,25 +25,29 @@ export default function ProductBrick() {
     }
 
     return (
-        <Link href={`/product/${id}`}>
             <div className="bg-white rounded-md m-5 flex flex-col align-center group">
-                <img 
-                    className="max-w-screen h-auto p-5 transition ease-linear group-hover:scale-110" 
-                    src="/images/hero-bg.png" 
-                />
+                <Link href={`/product/${id}`} className="flex flex-col align-center group">
+                    <img 
+                        className="max-w-screen h-auto p-5 transition ease-linear group-hover:scale-110" 
+                        src="/images/hero-bg.png" 
+                    />
 
-                <div className="text-center text-xl font-bold">
-                    {title}
-                </div>
-                <div className="text-center font-bold">
-                    {price}$
-                </div>
-                <button 
-                    className="rounded-full bg-cyan-500 text-white py-2 px-6 font-bold mx-auto transition ease-linear my-4 hover:bg-cyan-400"
-                    onClick={(event) => addToCart(event)}>
-                    BUY
-                </button>
+                    <div className="text-center text-xl font-bold">
+                        {title}
+                    </div>
+                    <div className="text-center font-bold">
+                        {price}$
+                    </div>
+                    <button 
+                        className="flex gap-2 items-center rounded-full bg-cyan-500 text-white py-2 px-6 font-bold mx-auto transition ease-linear my-4 hover:bg-cyan-400"
+                        onClick={(event) => addToCart(event)}>
+                        BUY
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                        </svg>
+
+                    </button>
+                </Link>
             </div>
-        </Link>
     )
 }
